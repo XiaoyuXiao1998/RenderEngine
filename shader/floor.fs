cfea3541c9cff035bd630dbbaef907254c4da2ef
xiao xiaoyv <954827761@qq.com>
2022-06-30 02:02:36 +0800

    Add project files.

---
+++ shader/floor.fs
@@ -0,0 +1,30 @@
+#version 330 core
+out vec4 FragColor;
+
+struct Material {
+    vec3 ambient;
+    vec3 diffuse;
+    vec3 specular;    
+    float shininess;
+}; 
+
+struct Light {
+    vec3 position;
+
+    vec3 ambient;
+    vec3 diffuse;
+    vec3 specular;
+};
+
+in vec3 FragPos;  
+in vec3 Normal;  
+  
+uniform vec3 viewPos;
+uniform Material material;
+uniform Light light;
+
+void main()
+{
+
+    FragColor = vec4(material.ambient, 1.0);
+} 
